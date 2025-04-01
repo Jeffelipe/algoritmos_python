93bcbd91309f1f8979781c900c2cef34e29af789
Jeferson Felipe <jfss16@aluno.ifal.edu.br>
2025-04-01 20:23:29 -0300

    Meu primeiro commit

---
+++ Aula 1/Tarefa - 1.py
@@ -0,0 +1,5 @@
+def saudar_mundo():
+    print("Olá, mundo!")
+
+if __name__ == "__main__":
+    saudar_mundo()
---
+++ Aula 1/Tarefa - 2.py
@@ -0,0 +1,4 @@
+numero = 0
+
+numero = input("Informe o número: ")
+print("O número informado foi: ", numero)
