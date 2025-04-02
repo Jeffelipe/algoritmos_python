72efbb33978ddc8e6f7c82e2452c607d0a5d7529
Jeferson Felipe <jfss16@aluno.ifal.edu.br>
2025-04-01 21:27:59 -0300

    Meu segundo commit

---
+++ Aula 1/Tarefa - 3.py
@@ -0,0 +1,9 @@
+# Solicitar dois números ao usuário
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+
+# Calcular a soma
+soma = numero1 + numero2
+
+# Imprimir o resultado
+print(f"A soma de {numero1} e {numero2} é {soma}")
