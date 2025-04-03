76d017b432140fe38d0814abc8df28abd2eef35a
Jeferson Felipe <jfss16@aluno.ifal.edu.br>
2025-04-02 21:34:47 -0300

    Quinto commit

---
+++ Aula 1/Tarefa - 7.py
@@ -0,0 +1,7 @@
+lado1 = float(input("Digite o primeiro lado: "))
+lado2 = float(input("Digite o segundo lado: "))
+
+area = lado1 * lado2
+dobro_area = area * 2
+
+print(f"A área do quadrado é {area} e dobro e área é {dobro_area}")
