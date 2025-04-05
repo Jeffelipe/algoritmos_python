c7da2ac282270dc31ad9253e0c61680a638758ad
Jeferson Felipe <jfss16@aluno.ifal.edu.br>
2025-04-04 21:45:35 -0300

    estrutura de decisão

---
+++ estruturadedecisao/Tarefa 01.py
@@ -0,0 +1,9 @@
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+
+if numero1 > numero2:
+    print("o maior número é: {numero1}.")
+elif numero2 > numero1:
+    print("o maior número é: {numero2}.")
+else:
+    print("os números são iguais.")
---
+++ estruturadedecisao/Tarefa 02.py
@@ -0,0 +1,8 @@
+numero = float(input("Digite o número: "))
+
+if numero > 0:
+    print("o número é: Positivo.")
+elif numero < 0:
+    print("o número é: Negativo.")
+else:
+    print("o número é: Neutro.")
