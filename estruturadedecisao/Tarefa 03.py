2c1470edd8a812bb34d096b9a50a7b94ac525674
Jeferson Felipe <jfss16@aluno.ifal.edu.br>
2025-04-07 21:45:08 -0300

    Segunda-feira

---
+++ estruturadedecisao/Tarefa 03.py
@@ -0,0 +1,12 @@
+sexo = 0
+sexo = input("Informe F para Femenino e M para Masculino: ")
+
+if sexo.upper() == "M":
+    print("MASCULINO")
+
+elif sexo.upper() == "F":
+    print("FEMENINO")
+
+else:
+    print("sexo Inválido")
+
---
+++ estruturadedecisao/Tarefa 04.py
@@ -0,0 +1,12 @@
+letra = input("Informe uma letra: ").upper()
+
+if  (letra == "A"
+    or letra == "E"
+    or letra == "I"
+    or letra == "O"
+    or letra == "U"):
+
+    print ("A letra digitada é uma Vogal")
+
+else:
+    print("A letra digitada é uma Consoante")
